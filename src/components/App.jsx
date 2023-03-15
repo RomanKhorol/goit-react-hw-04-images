55b6f7ef47c4654ab2a6099a1f84a963d1a42954
RomanKhorol <Rom4uk691@gmail.com>
2023-03-15 22:47:21 +0200

    fechComingButCatchAsFinal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,121 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import Notiflix from 'notiflix';
 
+import { fetchPictures } from './PicturesAPI';
+import Button from 'components/Button/Button';
+import { BallTriangle } from 'react-loader-spinner';
+import Modal from 'components/Modal/Modal';
 import Serchbar from './Searchbar/Serchbar';
 import ImageGllery from './ImageGallery/ImageGallery';
 
-export default class App extends Component {
-  state = {
-    text: '',
-    numberPage: 1,
+export default function App() {
+  const [pictures, setPictures] = useState([]);
+  const [button, setButton] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [shownModal, setShownModal] = useState(false);
+  const [urlLargeImg, setUrlLargeImg] = useState('');
+  const [text, setText] = useState('');
+  const [numberPage, setNumberPaget] = useState(1);
+
+  useEffect(() => {
+    if (text === '') {
+      return;
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    async function getPictures() {
+      try {
+        setPictures([]);
+        setButton(false);
+        setLoading(true);
+        const fetchedPictures = await fetchPictures(text, numberPage);
+        setPictures(prevPictures => [...prevPictures, fetchedPictures.hits]);
+        console.log(fetchedPictures);
+        if (fetchPictures.hits.length === 0) {
+          Notiflix.Notify.warning('No picture are found');
+          setButton(false);
+        } else setButton(true);
+      } catch (error) {
+        setButton(false);
+        Notiflix.Notify.warning(
+          'Were sorry, but youve reached the end of search results.'
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+    getPictures();
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [text, numberPage]);
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      setShownModal(false);
+    }
   };
-  hadleFormsubmit = text => {
-    this.setState({ text });
-    this.setState({ numberPage: 1 });
+  const openModal = url => {
+    setShownModal(true);
+    setUrlLargeImg(url);
   };
-  onBtnClick = () => {
-    this.setState(prevState => ({
-      numberPage: prevState.numberPage + 1,
-    }));
+
+  const closeModalByClickOverlay = event => {
+    if (event.currentTarget === event.target) {
+      this.setState({ shownModal: false });
+    }
   };
-  nandleBtnClick = number => {
-    this.setState({ numberPage: number });
+
+  const hadleFormsubmit = text => {
+    setText(text);
+    setNumberPaget(1);
   };
-  render() {
-    return (
-      <div>
-        <div
-          style={{
-            display: 'flex',
-            borderRadius: '4px',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            backgroundColor: 'blue',
-            gap: '10px',
-            alignItems: 'center',
-            height: '65px',
-            fontSize: 40,
-            color: '#010101',
-          }}
-        >
-          <Serchbar onSubmit={this.hadleFormsubmit} />
-        </div>
-        <div>
-          <ImageGllery
-            text={this.state.text}
-            page={this.state.numberPage}
-            onClick={this.onBtnClick}
-          />
-        </div>
+  const onBtnClick = () => {
+    setNumberPaget(prevNumberPaget => prevNumberPaget + 1);
+  };
+
+  //      const nandleBtnClick = number => {
+  //   this.setState({ numberPage: number });
+  // };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          borderRadius: '4px',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          backgroundColor: 'blue',
+          gap: '10px',
+          alignItems: 'center',
+          height: '65px',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Serchbar onSubmit={hadleFormsubmit} />
       </div>
-    );
-  }
+      <div>
+        <ImageGllery pictures={pictures} openModal={openModal} />
+        {shownModal && (
+          <Modal closeModal={closeModalByClickOverlay}>
+            <img src={urlLargeImg} alt="" width="100%" height="100%" />
+          </Modal>
+        )}
+        {loading && (
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass={{}}
+            wrapperStyle=""
+            visible={true}
+          />
+        )}
+        {button && <Button addPictures={onBtnClick} />}
+      </div>
+    </div>
+  );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,114 +1,22 @@
-import { Component } from 'react';
-import Notiflix from 'notiflix';
-import { BallTriangle } from 'react-loader-spinner';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import Modal from 'components/Modal/Modal';
-import { fetchPictures } from '../PicturesAPI';
-import Button from 'components/Button/Button';
+
 import { Ul } from './ImageGllery.styled';
-export default class ImageGllery extends Component {
-  state = {
-    pictures: [],
-    numberPage: 1,
-    button: false,
-    loading: false,
-    shownModal: false,
-    urlLargeImg: '',
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.text !== this.props.text) {
-      this.setState({ pictures: [] });
-    }
-    if (
-      prevProps.text !== this.props.text ||
-      prevProps.page !== this.props.page
-    ) {
-      this.setState({ button: false });
-      this.setState({ loading: true });
-      fetchPictures(this.props.text, this.props.page)
-        .then(pictures => {
-          this.setState(prevState => ({
-            pictures: [...this.state.pictures, ...pictures.hits],
-          }));
-          if (pictures.hits.length === 0) {
-            Notiflix.Notify.warning('No picture are found');
-            this.setState({ button: false });
-          } else this.setState({ button: true });
-        })
-        .catch(error => {
-          this.setState({ button: false });
-          Notiflix.Notify.warning(
-            'Were sorry, but youve reached the end of search results.'
+const ImageGllery = ({ pictures, openModal }) => {
+  return (
+    <div>
+      <Ul className="gallery">
+        {pictures.map(picture => {
+          return (
+            <ImageGalleryItem
+              key={picture.id}
+              pitureUrl={picture.webformatURL}
+              largeImageURL={picture.largeImageURL}
+              hendleImgClik={openModal}
+            ></ImageGalleryItem>
           );
-        })
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.setState({ shownModal: false });
-    }
-  };
-  openModal = url => {
-    this.setState({ shownModal: true });
-    this.setState({ urlLargeImg: url });
-  };
-
-  closeModalByClickOverlay = event => {
-    if (event.currentTarget === event.target) {
-      this.setState({ shownModal: false });
-    }
-  };
-
-  render() {
-    const { pictures, button, loading, shownModal } = this.state;
-
-    return (
-      <div>
-        <Ul className="gallery">
-          {pictures.map(hit => {
-            return (
-              <ImageGalleryItem
-                key={hit.id}
-                pitureUrl={hit.webformatURL}
-                largeImageURL={hit.largeImageURL}
-                hendleImgClik={this.openModal}
-              ></ImageGalleryItem>
-            );
-          })}
-        </Ul>
-        {shownModal && (
-          <Modal closeModal={this.closeModalByClickOverlay}>
-            <img
-              src={this.state.urlLargeImg}
-              alt=""
-              width="100%"
-              height="100%"
-            />
-          </Modal>
-        )}
-        {loading && (
-          <BallTriangle
-            height={100}
-            width={100}
-            radius={5}
-            color="#4fa94d"
-            ariaLabel="ball-triangle-loading"
-            wrapperClass={{}}
-            wrapperStyle=""
-            visible={true}
-          />
-        )}
-        {button && <Button addPictures={this.props.onClick} />}
-      </div>
-    );
-  }
-}
+        })}
+      </Ul>
+    </div>
+  );
+};
+export default ImageGllery;
